a71860120d3c106c7e9a30d9b7ace9167d08f78e
Jim Challenger <jim.challenger1@gmail.com>
2012-02-27 11:14:39 -0600

    provided basic navigation:

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,17 @@ class PagesController < ApplicationController
   def home
     @title = "Lucid Dreaming is Yours!"
   end
+  
+  def about
+    @title = "About"
+  end
+  
+  def resources
+    @title = 'Resources'
+  end
+  
+  def forums
+    @title = "Forums"
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ LucidDreaming::Application.routes.draw do
   resources :users
 
   root :to  => 'pages#home'
+  get '/about' => 'pages#about'
+  get '/resources' => 'pages#resources'
+  get '/forums'  => 'pages#forums'
   
 end
